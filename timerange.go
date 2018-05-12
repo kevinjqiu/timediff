5dc1394792643edfff904387c3083e811880cb89
Kevin Jing Qiu <kevin@idempotent.ca>
2018-05-12 15:53:33 -0400

    Implement sorting

--- timerange.go
+++ timerange.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"time"
 )
 
@@ -22,6 +23,26 @@ func (tr TimeRange) String() string {
 // TimeRange represents a list of TimeRanges
 type TimeRanges []TimeRange
 
+func (trs TimeRanges) Len() int {
+	return len(trs)
+}
+
+func (trs TimeRanges) Less(i, j int) bool {
+	if trs[i].start.Before(trs[j].start) {
+		return true
+	}
+	if trs[i].start.After(trs[j].start) {
+		return false
+	}
+	return trs[i].end.Before(trs[j].end)
+}
+
+func (trs TimeRanges) Swap(i, j int) {
+	trs[i], trs[j] = trs[j], trs[i]
+}
+
 func (trs TimeRanges) Subtract(other TimeRanges) TimeRanges {
+	sort.Sort(trs)
+	sort.Sort(other)
 	return TimeRanges{}
 }
