8892b06ab61297f22c99011950cb503d3568afe1
Kevin Jing Qiu <kevin@idempotent.ca>
2018-05-12 14:55:42 -0400

    Adapt to multiple time ranges

--- timerange.go
+++ timerange.go
@@ -1,7 +1,11 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+// TimeRange represents a single TimeRange with start and end time
 type TimeRange struct {
 	start time.Time
 	end   time.Time
@@ -9,4 +13,15 @@ type TimeRange struct {
 
 func (tr TimeRange) Subtract(other TimeRange) TimeRange {
 	return TimeRange{}
+}
+
+func (tr TimeRange) String() string {
+	return fmt.Sprintf("(%s - %s)", tr.start.Format("15:04"), tr.end.Format("15:04"))
+}
+
+// TimeRange represents a list of TimeRanges
+type TimeRanges []TimeRange
+
+func (trs TimeRanges) Subtract(other TimeRanges) TimeRanges {
+	return TimeRanges{}
 }
--- timerange_test.go
+++ timerange_test.go
@@ -14,37 +14,40 @@ func makeHHMM(timeStr string) time.Time {
 	return timeStruct
 }
 
-func makeTimeRangeHHMM(start string, end string) TimeRange {
-	return TimeRange{
-		makeHHMM(start),
-		makeHHMM(end),
+// Construct a TimeRanges object
+func makeTimeRangesHHMM(start string, end string) TimeRanges {
+	return TimeRanges{
+		TimeRange{
+			makeHHMM(start),
+			makeHHMM(end),
+		},
 	}
 }
 
 func TestTimeRange1SupersedesTimeRange2(t *testing.T) {
-	tr1 := makeTimeRangeHHMM("09:00", "10:00")
-	tr2 := makeTimeRangeHHMM("09:00", "09:30")
+	tr1 := makeTimeRangesHHMM("09:00", "10:00")
+	tr2 := makeTimeRangesHHMM("09:00", "09:30")
 	trdiff := tr1.Subtract(tr2)
-	assert.Equal(t, trdiff, makeTimeRangeHHMM("09:30", "10:00"))
+	assert.Equal(t, trdiff, makeTimeRangesHHMM("09:30", "10:00"))
 }
 
 func TestTimeRange1EqualsTimeRange2(t *testing.T) {
-	tr1 := makeTimeRangeHHMM("09:30", "10:30")
-	tr2 := makeTimeRangeHHMM("09:30", "10:30")
+	tr1 := makeTimeRangesHHMM("09:30", "10:30")
+	tr2 := makeTimeRangesHHMM("09:30", "10:30")
 	trdiff := tr1.Subtract(tr2)
 	assert.Equal(t, trdiff, TimeRange{})
 }
 
 func TestTimeRange1BoardersTimeRange2(t *testing.T) {
-	tr1 := makeTimeRangeHHMM("09:00", "09:30")
-	tr2 := makeTimeRangeHHMM("09:30", "15:00")
+	tr1 := makeTimeRangesHHMM("09:00", "09:30")
+	tr2 := makeTimeRangesHHMM("09:30", "15:00")
 	trdiff := tr1.Subtract(tr2)
-	assert.Equal(t, trdiff, makeTimeRangeHHMM("09:00", "09:30"))
+	assert.Equal(t, trdiff, makeTimeRangesHHMM("09:00", "09:30"))
 }
 
 func TestTimeRangesDoNotIntersect(t *testing.T) {
-	tr1 := makeTimeRangeHHMM("09:00", "09:30")
-	tr2 := makeTimeRangeHHMM("09:31", "15:00")
+	tr1 := makeTimeRangesHHMM("09:00", "09:30")
+	tr2 := makeTimeRangesHHMM("09:31", "15:00")
 	trdiff := tr1.Subtract(tr2)
-	assert.Equal(t, trdiff, makeTimeRangeHHMM("09:00", "09:30"))
+	assert.Equal(t, trdiff, makeTimeRangesHHMM("09:00", "09:30"))
 }
